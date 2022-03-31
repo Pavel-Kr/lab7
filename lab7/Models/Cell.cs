dd228410bdf465d382a85e626f5314f4e9921003
Павел <pkrivosheev@bk.ru>
2022-03-31 13:13:47 +0700

    Добавьте файлы проекта.

---
+++ lab7/Models/Cell.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Media;
+using System;
+
+namespace lab7.Models
+{
+    [Serializable()]
+    public class Cell
+    {
+        string mark;
+        public string Mark
+        {
+            get => mark;
+            set
+            {
+                mark = value;
+                switch (value)
+                {
+                    case "0":
+                        Background = Brushes.Red;
+                        break;
+                    case "1":
+                        Background = Brushes.Yellow;
+                        break;
+                    case "2":
+                        Background = Brushes.Green;
+                        break;
+                    default:
+                        mark = "#ERROR";
+                        Background = Brushes.White;
+                        break;
+                }
+            }
+        }
+        [field: NonSerialized()]
+        public IBrush Background { get; set; }
+        public Cell(string mark)
+        {
+            Mark = mark;
+        }
+    }
+}
